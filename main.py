ba0f1b5d57626136204f0278945dbac7f26e2a7c
penguinyaro <jbwmyoung@gmail.com>
2022-10-21 20:18:06 -1000

    Refractoring for pure python modules to `squid`
    Cleaner structure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from data import url # TODO fix module setup so we have a name to import these things (need to put these in another directory)
-from viz import viz
+from squid.data import url
+from squid.viz import viz
 
 
 # TODO actually make a main funciton and/or wrap this into flask implementation
