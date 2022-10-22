bd3286db6f94d356b388f125595707ae48bd2af9
penguinyaro <jbwmyoung@gmail.com>
2022-10-21 17:59:06 -1000

    Updaated flask_hello with more routes

--- site/flask_hello.py
+++ site/flask_hello.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'goHello, World'
+
+@app.route('/<name>')
+def chicken(name):
+    return f"Hello, {escape(name)}!"
