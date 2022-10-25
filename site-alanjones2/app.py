31533ce58389ba585096430c8b4f56d3f36c4706
penguinyaro <jbwmyoung@gmail.com>
2022-10-25 22:37:42 +0900

    First working plot of our own!

--- site-alanjones2/app.py
+++ site-alanjones2/app.py
@@ -6,13 +6,25 @@ import json
 import plotly
 import plotly.express as px
 
-print("__name__: " + __name__)
+# Bug 14 fix: https://www.geeksforgeeks.org/python-import-from-parent-directory/
 
-# from ..brains.data.url as url
-#
-# import brains.data.url as url
-# from brains.viz import plotly_plots as pp
+import sys
+import os
 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
+from brains.viz import plotly_viz as pp
+from brains.data import url as url
 
 
 app = Flask(__name__)
@@ -29,16 +41,17 @@ def index():
 
 
 def gm(country='United Kingdom'):
-    df = pd.DataFrame(px.data.gapminder())
-    # path = "sample_data/example.csv"
-    # df = url.path_to_dataframe(path)
+    # df = pd.DataFrame(px.data.gapminder())
+    path = "../sample_data/example.csv"
+    df = url.path_to_dataframe(path)
 
-    # fig = pp.histogram(df['Age'])
+    fig = pp.histogram(df['Age'])
 
-    fig = px.line(df[df['country'] == country], x="year", y="gdpPercap")
+    # fig = px.line(df[df['country'] == country], x="year", y="gdpPercap")
 
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    print(fig.data[0])
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    # print(fig.data[0])
     # fig.data[0]['staticPlot']=True
 
-    return graphJSON
+    # return graphJSON
+    return fig
