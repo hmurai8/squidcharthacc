6919f2c275c2f165359fad79c9180a7ab621af85
hmurai8 <115200233+hmurai8@users.noreply.github.com>
2022-10-27 12:04:26 -1000

    trying out pivot table

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ def data_summarize(df):
     current_columns = df.columns #list USE THIS
     types = df.dtypes
     unique = df.nunique()
-    description = df.describe()
-    total = df.sum(numeric_only = True)
     value_index = 0
+    total = df.sum(numeric_only=True)
     agg = df.describe().loc[['count','max', 'min', 'mean']]
 
     print("Current columns:")
@@ -36,24 +35,15 @@ def data_summarize(df):
     print(types)
     print("Object Count")
     print("Numeric data aggregation:")
-    print(description)
-    print("Sum of numeric data:")
     print(agg)
-    max_index_tot = len(total) - 1
+    print("Sum of numeric data:")
+    print(len(total))
     print(total)
-    # for value in total:
-    #     column_name = total[value_index] # NOT columns use total (need to make list)
-    #     print("Sum of values ==> " + str(value))
-    #     while value_index < max_index_tot:
-    #         value_index += 1
     print("* Suggested column fields: 10 unique values or less *")
-    print(len(unique))
-    max_index_uni = len(unique) - 1
     for value in unique:
         column_name = current_columns[value_index] # Could use columns here or make list unique
-        print("Number of unique values in " + str(column_name) + " column ==> " + str(value))
-        while value_index < max_index_uni:
-            value_index += 1
+        print("Number of unique values in " + str(column_name) + " ==> " + str(value))
+        value_index += 1
 
     print(unique)
     print("Types of displays possible:")
@@ -63,10 +53,16 @@ data_summarize(df)
 
 # User should actually should choose fields; those get put in as parameters
 def pivot_table(df):
-    piv_tab = pd.pivot_table(df, values="Age", index=["Name", "Zip Code"], columns=["Gender"], aggfunc="sum")
+    piv_tab = pd.pivot_table(
+        df,
+        values=["Age"],
+        index=["Name", "Zip Code"],
+        columns=["Gender"],
+        aggfunc="sum"
+    )
     return piv_tab
 
-print("Pivot Table")
+print("--- Pivot Table ---")
 print(pivot_table(df))
 
 #just trying out things
