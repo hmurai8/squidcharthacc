e8a18dc53f7ed892c7ff2449a8e4ba8b0da00b39
hmurai8 <115200233+hmurai8@users.noreply.github.com>
2022-10-27 22:46:09 -1000

    Updated data summary + draft workflow

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import brains.viz.viz as viz
 
 # Load data
 # TODO prompt user to designate path
-path = "sample_data/example.csv"
-df = url.path_to_dataframe(path)
+#path = "sample_data/example.csv"
+#df = url.path_to_dataframe(path)
 
 # Display summary of data
 # Also tutorial for user as he goes along (suggesting what values to choose in pivot table
@@ -19,38 +19,43 @@ df = url.path_to_dataframe(path)
 #TODO make module/function for this
 # e.g. data.summarize(df)
 
+# User prompted to enter dataset
+# User enters dataset url
+path = input("Please enter the csv url for data visualization: \n")
+df = url.path_to_dataframe(path)
+
+# Function to summarize dataset
 def data_summarize(df):
     import pandas as pd
     pd.set_option('display.float_format', lambda x: '%.0f' % x)
-    current_columns = df.columns #list
-    types = df.dtypes
-    unique = df.nunique()
-    total = df.sum(numeric_only=True)
-    agg = df.describe().loc[['count','max', 'min', 'mean']]
-    value_index = 0
-
-    print("Current columns:")
-    print(', '.join(current_columns))
+    print("General Information")
+    print(df.info())
+    print("Columns:")
+    print(', '.join(df.columns))
     print("Types of data:")
-    print(types)
+    print(df.dtypes)
     print("Object Count")
     print("Numeric data aggregation:")
-    print(agg)
-    print("Sum of numeric data: " + str((len(total))) + " columns")
-    print(total)
+    print(df.describe().loc[['count','max', 'min', 'mean']])
+    print("Sum of numeric data: ")
+    print(df.sum(numeric_only=True))
     print("Number of unique values for all columns:")
-    for value in unique:
-        column_name = current_columns[value_index]
-        print(str(value) + " unique values in " + str(column_name))
-        value_index += 1
+    print(df.nunique())
     print("* Suggested column fields: 10 unique values or less *")
     print("Types of displays possible:")
     print("Bar chart, Histogram, Pie chart, Line graph")
 
+# User views data summary
 data_summarize(df)
 
-# ADD user input
-# User should actually should choose fields; those get put in as parameters
+# User chooses fields for pivot table
+print("From the current columns listed above, choose your fields for your pivot table.")
+print("Please enter it exactly how it is shown under Columns.")
+user_values = input("Please enter values for your pivot table: ")
+user_index = input("Please enter the index for your pivot table: ")
+user_columns = input("Please enter columns for your pivot table: ")
+
+# Pivot table generated based off of user input (if user inputted correctly)
 def pivot_table(df, values, index, columns, aggfunc):
     piv_tab = pd.pivot_table(
         df, values=values,
@@ -62,18 +67,23 @@ def pivot_table(df, values, index, columns, aggfunc):
 
 print("--- Pivot Table ---")
 print(pivot_table(df,
-        values=["Age"],
-        index=["Name"],
-        columns=["Gender"],
+        values=[user_values],
+        index=[user_index],
+        columns=[user_columns],
         aggfunc="sum"))
 
-#just trying out things
+# Prompt user to enter type of data visualization
+user_graph = input("Please enter the type of graph you would like to generate: \n")
+
+# Just one of the visualization options -- Bar Chart
+# User then generates one of the visualization options
 import plotly.express as px
 def bar_chart(df, x, y, title):
     fig = px.bar(df,x=x,y=y,title=title)
     fig.show()
-#not sure how to make parameters work
-bar_chart(df, x = "Age", y = "Gender", title = "Bar Chart")
+
+bar_chart(df, x = user_columns, y = user_values, title = "Bar Chart")
+# Customize title option??
 
 # Prompt user for input based on above
 # TODO implement this
